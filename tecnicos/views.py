34ad5fb4fd6c39bdfb0533d86c39cdc2bef3393e
Jeniffer713 <jeniffer.sangucho2052@utc.edu.ec>
2026-06-30 22:00:47 -0500

    Configuracion static files y estructura templates base Django

--- tecnicos/views.py
+++ tecnicos/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 def inicio(request):
-    return HttpResponse("Hola Sistema Tecnicos funcionando")
+    return render(request, 'inicio.html')
